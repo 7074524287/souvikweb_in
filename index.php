8749ab826c68373b130cebc524a314ede90ad822
7074524287 <singhasouvik823@gmail.com>
2024-07-18 22:49:43 +0530

    V1.0.0

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet"href="./assets/css/index.css">
+</head>
+<body>
+    <!-- <h2 style=" color:rgb(167, 95, 95)">Fill the from properly</h2>
+    <h2 >Fill the from properly</h2>
+    <h2 >Fill the from properly</h2> -->
+  
+    <h2>Fill the from properly</h2>
+    <h3>Fill the from properly</h3>
+    <h4>Fill the from properly</h4>
+    <h2>Fill the from properly</h2>
+    <h2>Fill the from properly</h2>
+    
+    
+    <h4>(please put correct information )</h4>
+    <from>
+        <input type="text"placeholder="username" id="un">
+        <br><br>
+        <input type="password"placeholder="password" id="ps" >
+        <br><br>
+        <button> <a href="/education.html">submit</a></button>
+        <div class="myclass"></div>
+        <div id="myid"></div>
+        <div id="mybox"></div>
+    </from>
+    <!-- <div id="box">
+        this is a example
+    </div> -->
+    <!-- <h1 class="heading">heading 1</h1>
+    <h2 class="heading">heading 2</h2>
+    <h3 class="heading">heading 3</h3>
+    <h4 class="heading">heading 4</h4> -->
+    <!-- <button style=" background-color:pink">click me</button> -->
+</body>
+</html>
